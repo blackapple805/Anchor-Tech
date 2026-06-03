41757bd81ba988b3990ed22bb6957e17a83b062f
Blackapple805 <blackapple805@gmail.com>
2026-06-03 13:17:12 -0700

    Switch to HashRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'node:fs'
-
-const certExists = fs.existsSync('./localhost+1-key.pem') && fs.existsSync('./localhost+1.pem')
 
 export default defineConfig({
-     plugins: [react()],
-     base: '/Anchor-Tech/',
-   })
+  plugins: [react()],
+  base: '/Anchor-Tech/',
+})
